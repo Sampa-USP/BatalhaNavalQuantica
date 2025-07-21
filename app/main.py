0d7858a1a4ef3d7ccdb7ea2ccbe1ccb1ebb811c9
Nara <nara.moraes@usp.br>
2025-07-21 18:19:13 -0300

    Update files

--- app/main.py
+++ app/main.py
@@ -1,10 +1,28 @@
-# app.py
-from flask import Flask
+from flask import Flask, jsonify, request, make_response
+
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Olá do servidor EC2!"
+def index():
+    return "Olá do servidor EC2!", 200
+
+@app.route("/texto")
+def texto():
+    return "Resposta em texto simples", 200
+
+@app.route("/json")
+def json_endpoint():
+    data = {"mensagem": "Esta é uma resposta JSON", "status": "ok"}
+    return jsonify(data), 200
+
+@app.route("/echo", methods=["POST"])
+def echo():
+    conteudo = request.json
+    return jsonify(recebido=conteudo), 200
+
+@app.route("/erro")
+def erro():
+    return make_response("Erro intencional", 500)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=80)
--- infraestructure/ec2-flask/outputs.tf
+++ infraestructure/ec2-flask/outputs.tf
@@ -1,3 +1,8 @@
 output "public_ip" {
   value = aws_instance.flask_server.public_ip
 }
+
+output "api_gateway_invoke_url" {
+  value = aws_apigatewayv2_stage.default.invoke_url
+  description = "URL pública para acessar a aplicação Flask via API Gateway HTTPS"
+}
