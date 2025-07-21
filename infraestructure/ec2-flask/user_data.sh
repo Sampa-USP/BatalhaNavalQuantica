e0c80fe9bbe3e1321ce0537d1303018cf6fca0ee
Nara <nara.moraes@usp.br>
2025-07-21 17:41:01 -0300

    Update files

---
+++ infraestructure/ec2-flask/user_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt update -y
+apt install -y docker.io
+systemctl start docker
+systemctl enable docker
+
+# Clone seu repositório
+git clone https://github.com/Sampa-USP/QuantumComputingBatalhaNaval.git /home/ubuntu/app
+cd /home/ubuntu/app/app
+
+# Build e run
+docker build -t flask-app .
+docker run -d -p 80:5000 flask-app
