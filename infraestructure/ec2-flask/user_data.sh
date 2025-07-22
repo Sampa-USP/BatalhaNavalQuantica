2b06790a8e68c1a62e02bb99cebf518d2474f8cd
Nara <nara.moraes@usp.br>
2025-07-22 13:14:42 -0300

    Fix

--- infraestructure/ec2-flask/user_data.sh
+++ infraestructure/ec2-flask/user_data.sh
@@ -9,5 +9,5 @@ git clone https://github.com/Sampa-USP/QuantumComputingBatalhaNaval.git /home/ub
 cd /home/ubuntu/app/app
 
 # Build e run
-docker build -t flask-app .
+docker build -t flask-app app
 docker run -d -p 80:5000 flask-app
